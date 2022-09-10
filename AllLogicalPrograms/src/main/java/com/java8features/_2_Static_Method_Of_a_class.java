2722d0aa35292e8e1eec60919d8413c556bc9ea4
amargithub777 <amarbhairat25@gmail.com>
2022-09-10 13:35:48 +0530

    First Project added

---
+++ AllLogicalPrograms/src/main/java/com/java8features/_2_Static_Method_Of_a_class.java
@@ -0,0 +1,22 @@
+package com.java8features;
+
+import java.util.function.BiFunction;
+
+//Four types of method references
+//1. Method reference to an instance method of an object – object::instanceMethod
+//2. Method reference to a static method of a class – Class::staticMethod
+//3. Method reference to an instance method of an arbitrary object of a particular type – Class::instanceMethod
+//4. Method reference to a constructor – Class::new
+
+//2. Method reference to a static method of a class – Class::staticMethod
+
+public class _2_Static_Method_Of_a_class {
+
+	public static void main(String[] args) {
+	//	BiFunction<Integer,Integer,Integer>
+//		BiFunction<Integer,Integer,Integer> product=Multiplication::multiply;
+//		int pr =product.apply(11,10);
+//		System.out.println("Product of given number is :"+pr);
+	}
+
+}
